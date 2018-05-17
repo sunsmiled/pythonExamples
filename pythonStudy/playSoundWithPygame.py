53ae0f93c17201df9f44faf53b98f1f64aeeb5cf
sunsmiled <smilesunf@163.com>
2018-05-17 23:53:32 +0800

    addMusic

--- pythonStudy/playSoundWithPygame.py
+++ pythonStudy/playSoundWithPygame.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Sun Fang'
 
+
+# # 播放声音
+# import pygame
+# import sys
+# pagame.init()
+# pygame.mixer.init()
+#
+# screen = pygame.display.set_mode([640, 480])
+# pygame.time.delay(1000)
+# splat = pygame.mixer.Sound("")
+# splat.play()
+#
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+
+# 播放音乐
 import pygame
 import sys
-pagame.init()
+pygame.init()
 pygame.mixer.init()
 
 screen = pygame.display.set_mode([640, 480])
 pygame.time.delay(1000)
-splat = pygame.mixer.Sound("")
-splat.play()
+
+pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play()
 
 while 1:
     for event in pygame.event.get():
